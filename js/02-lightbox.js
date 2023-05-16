b501054c7c0764648b05eb8787a1bd8f53ab38a3
Andriy2016 <122678717+Andriy2016@users.noreply.github.com>
2023-05-16 14:48:25 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+  };
+  
+  const galleryItemsMarksup = createGalleryItemsMarksup(galleryItems);
+  
+  refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarksup);
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  
+  function createGalleryItemsMarksup(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+      })
+      .join('');
+  }
